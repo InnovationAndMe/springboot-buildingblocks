e8e9f58a218160ad5aa2d351c084692bd53bc9c9
Raka Banerjee <raka.banerjee15@gmail.com>
2026-06-16 21:31:58 +0100

    First Commit

--- src/main/java/com/stacksimplify/restservices/entities/User.java
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.hateoas.RepresentationModel;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,6 +18,7 @@ import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
+@JsonIgnoreProperties({"firstname","lastname"})
 public class User extends RepresentationModel<User> { //ResourceSupport deprecated now
 
 	@Id
@@ -39,6 +43,7 @@ public class User extends RepresentationModel<User> { //ResourceSupport deprecat
 	private String role;
 	
 	@Column(name = "SSN", length=50, nullable=false, unique=true)
+	@JsonIgnore
 	private String ssn;
 
 	@OneToMany(mappedBy="user")
