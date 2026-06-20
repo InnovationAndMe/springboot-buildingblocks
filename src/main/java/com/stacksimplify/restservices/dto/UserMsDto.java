a9905f8e259180affe12d5d64889629f3a384bd7
Raka Banerjee <raka.banerjee15@gmail.com>
2026-06-20 01:21:07 +0100

    MapStruct - FirstCommit

---
+++ src/main/java/com/stacksimplify/restservices/dto/UserMsDto.java
@@ -0,0 +1,62 @@
+package com.stacksimplify.restservices.dto;
+
+public class UserMsDto {
+
+	private Long id;
+	private String userName;
+	private String emailAddress;
+	
+	
+	public UserMsDto() {
+	
+	}
+	
+	
+	
+	public UserMsDto(Long userid, String userName, String emailAddress) {
+		super();
+		this.id = userid;
+		this.userName = userName;
+		this.emailAddress = emailAddress;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+
+	public String getEmail() {
+		return emailAddress;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.emailAddress = email;
+	}
+
+	
+	
+	
+}
